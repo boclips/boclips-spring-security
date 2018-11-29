63565fd36115bbea37c67d0c4ab5a4f002344b0a
Matt Jones <matt@boclips.com>
2018-11-29 16:26:57 +0000

    Workarround for bean duplication issue
    Signed-off-by: José Carlos Valero Sánchez <josecarlos@boclips.com>

--- src/main/kotlin/com/boclips/security/keycloak/KeycloakSecurityConfig.kt
+++ src/main/kotlin/com/boclips/security/keycloak/KeycloakSecurityConfig.kt
@@ -1,19 +1,28 @@
 package com.boclips.security.keycloak
 
 import com.boclips.security.HttpSecurityConfigurer
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.FilterType
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.session.SessionRegistryImpl
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 
-@KeycloakConfiguration
 @Configuration
+//Keycloak bug: https://issues.jboss.org/browse/KEYCLOAK-8725
+@ComponentScan(
+        basePackageClasses = [KeycloakSecurityComponents::class],
+        excludeFilters = [
+            ComponentScan.Filter(type = FilterType.REGEX, pattern = ["org.keycloak.adapters.springsecurity.management.HttpSessionManager"])
+        ])
+@EnableWebSecurity
 class KeycloakSecurityConfig(val httpSecurityConfigurer: HttpSecurityConfigurer) : KeycloakWebSecurityConfigurerAdapter() {
     /**
      * Registers the KeycloakAuthenticationProvider with the authentication manager.
