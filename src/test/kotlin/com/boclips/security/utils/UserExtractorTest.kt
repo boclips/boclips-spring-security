aba0976f6b21f83235a066805c879890ab15c272
Benjamin Gröhbiel <ben@boclips.com>
2019-03-13 13:38:45 +0000

    Rename and rework test examples for clarity
    - Don't use User.fromEmail in assertions, because that's part of what's
      being tested.
    - Appease IntelliJ by making some things private
    - Remove superfluous assignments, shadowed variables, global test vars.
    
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- src/test/kotlin/com/boclips/security/utils/UserExtractorTest.kt
+++ src/test/kotlin/com/boclips/security/utils/UserExtractorTest.kt
@@ -10,27 +10,27 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.context.SecurityContextImpl
 
 internal class UserExtractorTest {
-    val boclipsUsername = "test@boclips.com"
-    val boclipsUserId = "user-id"
-
     @Test
-    fun `when userDetails extracts mail from username`() {
-        val boclipsUsername = "test@boclips.com"
-        setSecurityContext(org.springframework.security.core.userdetails.User(boclipsUsername, "password", emptyList()))
-
-        assertThat(UserExtractor.getCurrentUser()).isEqualTo(User.fromEmail(boclipsUsername, boclipsUsername))
+    fun `retrieves and identifies Boclips employees, using email as ID`() {
+        setSecurityContext(org.springframework.security.core.userdetails.User("test@boclips.com", "password", emptyList()))
+        assertThat(UserExtractor.getCurrentUser()).isEqualTo(User(boclipsEmployee = true, id = "test@boclips.com"))
     }
 
     @Test
-    fun `when keycloak principal extracts preferredUsername`() {
-        val keycloakPrincipal = KeycloakPrincipal(boclipsUserId, KeycloakSecurityContext(null, AccessToken().apply { preferredUsername = boclipsUsername }, null, null))
-        setSecurityContext(keycloakPrincipal)
-
-        assertThat(UserExtractor.getCurrentUser()).isEqualTo(User.fromEmail(boclipsUsername, boclipsUserId))
+    fun `uses a keycloak principal's preferred username`() {
+        setSecurityContext(KeycloakPrincipal(
+                "my-user-id",
+                KeycloakSecurityContext(
+                        null,
+                        AccessToken().apply { preferredUsername = "test@noclips.com" },
+                        null,
+                        null
+                )))
+        assertThat(UserExtractor.getCurrentUser()).isEqualTo(User(boclipsEmployee = false, id = "my-user-id"))
     }
 
     @Test
-    fun `when there is no user in current context`() {
+    fun `is null when no user is in current context`() {
         setSecurityContext(null)
         assertThat(UserExtractor.getCurrentUser()).isNull()
 
@@ -38,7 +38,7 @@ internal class UserExtractorTest {
         assertThat(UserExtractor.getCurrentUser()).isNull()
     }
 
-    fun setSecurityContext(authenticatedUser: Any?) {
+    private fun setSecurityContext(authenticatedUser: Any?) {
         SecurityContextHolder.setContext(SecurityContextImpl(TestingAuthenticationToken(authenticatedUser, null)))
     }
 }
